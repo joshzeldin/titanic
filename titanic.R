19c1670a0ed5db6b1fbc4b687abf5856c0d1794b
Joshua Zeldin <jzeldin@Joshuas-MacBook-Pro.local>
2017-02-28 21:52:13 -0500

    Adds the column Deck to the combined data set.

--- titanic.R
+++ titanic.R
@@ -5,7 +5,27 @@
 #####################################################################################
 
 #####################################################################################
-#Reading in the Data
+#Reading and Overview of the Data
 #####################################################################################
+
+library("rpart")
+
+#####################################################################################
+#Reading and Overview of the Data
+#####################################################################################
+
 train <- read.csv("data/train.csv")
 test <- read.csv("data/test.csv")
+str(train)
+summary(train)
+str(test)
+summary(test)
+
+#####################################################################################
+#Add New Values to the Dataset
+#####################################################################################
+
+#We can take the first letter of the Cabin to get the deck
+train$Deck<-substr(train$Cabin,1,1)
+#There are cabins A-G and a single Cabin T
+table(train$Deck)
